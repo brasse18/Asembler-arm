6c2a283b2cb5c615cdfcc1f663501a2119bf1982
brasse18 <brasse.bb@gmail.com>
2017-05-11 17:11:30 +0000

    added wait function

--- labfiles/interrupt/src/counter_c.c
+++ labfiles/interrupt/src/counter_c.c
@@ -2,49 +2,85 @@
 #include <linux/kernel.h>		// Needed for KERN_INFO
 #include <linux/interrupt.h>	// Needed for register_irq
 
+//#include <linux/unistd.h>
+
 #define IRQ 0	// Byt ut 0 mot rätt IRQ
+bool beenRun = false;
+
+
+void set_high( void );
+
+void set_low( void );
+
 
 /*
- * Enables access to the gpio
- */
+* Enables access to the gpio
+*/
 void setup( void );
+
 /*
- * Disables access to the GPIO
- */
+* Disables access to the GPIO
+*/
 void setdown( void );
+
 /*
- * Interrupt service routine
- */
+* Interrupt service routine
+*/
 irqreturn_t counter_isr(int,void*);
 
 /*
- * Module init function
- */
+* Module init function
+*/
+static void __exit btnrpi_exit(void);
+
 static int __init btnrpi_init(void)
 {
-	setup();
-	if(request_irq(IRQ, counter_isr, IRQF_TRIGGER_FALLING, "counter#btn", NULL)){
-		printk(KERN_ERR "COUNTER - Unable to request IRQ. Aborting.\n");
-		free_irq(IRQ,NULL);
-		return -EINVAL;
-	}
-	printk(KERN_INFO "COUNTER - Driver initialized.\n");
-	return 0;
+  int i = 0;
+
+
+  if (!beenRun)
+  {
+    setup();
+    set_high();
+
+    printk(KERN_INFO "COUNTER - Driver initialized BEFORE LOOP.\n");
+
+    for (i = 0; i < 100000; i++)
+    {
+      printk(KERN_INFO "LOOP: %d .\n", i);
+      i++;
+      i--;
+    }
+    set_low();
+    printk(KERN_INFO "COUNTER - Driver initialized.\n");
+    beenRun = true;
+  }
+  /*
+  if(request_irq(IRQ, counter_isr, IRQF_TRIGGER_FALLING, "counter#btn", NULL)){
+  printk(KERN_ERR "COUNTER - Unable to request IRQ. Aborting.\n");
+  free_irq(IRQ,NULL);
+  return -EINVAL;
+}
+*/
+
+//btnrpi_exit();
+return 0;
+
 }
 
 /**
- * Module exit function
- */
+* Module exit function
+*/
 static void __exit btnrpi_exit(void)
 {
-	printk(KERN_INFO "COUNTER - Unloading driver\n");
-	setdown();
-	free_irq(IRQ, NULL);	
+  printk(KERN_INFO "COUNTER - Unloading driver\n");
+  setdown();
+  free_irq(IRQ, NULL);
 }
 
 MODULE_LICENSE("GPLv3");
 MODULE_DESCRIPTION("Linux LED driver");
-MODULE_AUTHOR("ERA NAMN HÄR");
+MODULE_AUTHOR("Viktor Enfeldt, Björn Blomberg");
 
 module_init(btnrpi_init);
 module_exit(btnrpi_exit);
